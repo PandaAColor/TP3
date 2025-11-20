f62d506542cdafed61af7de0b5bbce01f5206def
Sol <pandaacolor@hotmail.com>
2025-11-20 13:06:03 -0300

    personaje

---
+++ personaje.py
@@ -0,0 +1,7 @@
+
+class Personaje:
+
+    def __init__(self, nombre = str, supernombre = str, genero = str):
+        self.nombre = nombre
+        self.snombre = supernombre
+        self.genero = genero
